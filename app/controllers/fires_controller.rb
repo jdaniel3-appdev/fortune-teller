c601c9843f4e0ff183c323449f3f38fd89497e9a
RSCosworth4x4 <jdaniel3@chicagobooth.edu>
2021-02-01 16:11:28 +0000

    Commit 2 Working up to Libra

--- app/controllers/fires_controller.rb
+++ app/controllers/fires_controller.rb
@@ -1,6 +1,6 @@
 class FiresController < ApplicationController
   def ram
-    @horoscope1 = "As your professional dreams unfold, Aries, you may worry about the downside. First, there are new responsibilities that you might doubt your ability to fulfill. Second, you might be catapulted into an uncomfortable new realm of office politics".to_s
+    @horoscope1 = "As your professional dreams unfold, Aries, you may worry about the downside. First, there are new responsibilities that you might doubt your ability to fulfill. Second, you might be catapulted into an uncomfortable new realm of office politics. "
     @horoscope2 = "Don't let these matters put a damper on your enthusiasm. You have what it takes to fulfill the first concern and the wisdom to avoid the second. Onward and upward.".to_s
     
     @array_of_numbers = Array.new
